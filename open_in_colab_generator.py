58d6d4a56c13d6b7bbcf5d0d494723e2870294a0
Islam Ahmed <eslam760000@gmail.com>
2023-07-20 09:51:34 +0300

    Update open_in_colab_generator.py

--- open_in_colab_generator.py
+++ open_in_colab_generator.py
@@ -21,7 +21,8 @@ colab_link = colab_link.replace(".com","") # Remving .com
 
 binder_link = '/'.join(colab_link.replace("/github","").replace('/tree','').split('/')[1:3]) + "/" + branch
 
-
+codespace_link = '/'.join(colab_link.replace("/github","").replace('/tree','').split('/')[1:3])
+githubdev_link = github_link.replace('github.com','github.dev')
 
 
 ############
@@ -98,4 +99,25 @@ with col2:
 
 st.markdown(livebook_markdown)
 
+############
+st.divider()
+############
 
+st.write("### Github Codespace")
+quickstart = "?quickstart=1" if st.checkbox("Quick Start",help="Automatically create or reuse the most recent matching codespaces") else ""
+is_file = st.checkbox("Is File",help="Is it File or Repository")
+
+codespace_markdown = f'[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)]({f"https://codespaces.new/{codespace_link}{quickstart}" if not is_file else githubdev_link})'
+codespace_html = f'''<a href={f'https://codespaces.new/{codespace_link}{quickstart}' if not is_file else githubdev_link}><img src='https://github.com/codespaces/badge.svg' alt='Open in GitHub Codespaces' style='max-width: 100%;'></a>'''
+
+# Showing Codes
+col1 , col2 = st.columns(2)
+with col1:
+    st.write("**Markdown**")
+    st.code(codespace_markdown)
+    
+with col2:
+    st.write("**HTML**")
+    st.code(codespace_html)
+
+st.markdown(codespace_markdown)
